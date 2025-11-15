0f7903f87258a01c435a5cd92e104eed8c4372e0
tanvir <tanvirislam173389@gmail.com>
2025-11-15 16:15:55 +0600

    added allbook page responsive

--- src/Component/Allbooktable.jsx
+++ src/Component/Allbooktable.jsx
@@ -4,31 +4,31 @@ import { Link } from "react-router";
 export default function Allbooktable({ book }) {
     return (
         <tr className="border-b border-gray-200 hover:bg-linear-to-r from-blue-50 to-indigo-50 transition-all duration-200 cursor-pointer">
-            <td className="py-4 px-5">
+            <td className="py-2 px-3 md:py-4 md:px-5">
                 <div className="relative w-14 h-20">
                     <img
                         src={book.coverImage}
                         alt={book.title}
-                        className="w-full h-full object-cover rounded-lg shadow-sm hover:shadow-md transition-all duration-300"
+                        className="w-full h-full object-cover rounded-lg hover:shadow-md transition-all duration-300"
                     />
                 </div>
             </td>
-            <td className="py-4 px-5 font-semibold text-gray-800 hover:text-blue-600 transition-colors duration-200">
+            <td className="py-2 px-3 md:py-4 md:px-5 text-sm md:text-base font-semibold text-gray-800 hover:text-blue-600 transition-colors duration-200">
                 {book.title}
             </td>
-            <td className="py-4 px-5 text-gray-600 italic">{book.author}</td>
-            <td className="py-4 px-5">
-                <span className="bg-blue-100 text-blue-700 text-sm font-medium px-3 py-1 rounded-full border border-blue-200 shadow-sm">
+            <td className="py-2 px-3 md:py-4 md:px-5 text-sm md:text-base text-gray-600 italic">{book.author}</td>
+            <td className="py-2 px-3 md:py-4 md:px-5">
+                <span className="bg-blue-100 text-blue-700 text-[12px] md:text-sm font-medium px-3 py-1 rounded-full border border-blue-200 shadow-sm">
                     {book.genre}
                 </span>
             </td>
-            <td className="py-4 px-5 text-yellow-600 font-semibold flex items-center gap-1">
+            <td className="py-2 px-3 md:py-4 md:px-5 text-yellow-600 text-sm md:text-base font-semibold flex items-center gap-1">
                 <FaStar className="text-yellow-500" /> {book.rating} / 5
             </td>
-            <td className="py-4 px-5">
+            <td className="py-2 px-3 md:py-4 md:px-5">
                 <Link
                     to={`/details/${book._id}`}
-                    className="bg-linear-to-r from-blue-500 to-indigo-600 text-white px-4 py-1.5 rounded-full text-sm font-semibold shadow hover:from-blue-600 hover:to-indigo-700 transition-all duration-300"
+                    className="bg-linear-to-r from-blue-500 to-indigo-600 text-[12px] md:text-sm text-white px-4 py-1.5 rounded-full font-semibold shadow hover:from-blue-600 hover:to-indigo-700 transition-all duration-300"
                 >
                     View
                 </Link>
--- src/Pages/AllBooks.jsx
+++ src/Pages/AllBooks.jsx
@@ -13,20 +13,20 @@ export default function AllBooks() {
     }, []);
 
     return (
-        <div className="bg-white rounded-xl shadow-md p-6 overflow-x-auto">
-            <h2 className="text-2xl font-bold mb-6">All Books</h2>
+        <div className="bg-white py-6 overflow-x-auto">
+            <h2 className="md:text-2xl font-bold px-2 md:px-6 mb-2 md:mb-6">All Books</h2>
 
             <table className="min-w-full border-collapse">
                 <thead>
-                    <tr className="border-b border-gray-300 text-left text-gray-700">
-                        <th className="py-3 px-4 font-semibold">COVER</th>
-                        <th className="py-3 px-4 font-semibold">TITLE</th>
-                        <th className="py-3 px-4 font-semibold">AUTHOR</th>
-                        <th className="py-3 px-4 font-semibold">GENRE</th>
-                        <th className="py-3 px-4 font-semibold flex items-center gap-1">
+                    <tr className="border-b border-gray-300 text-[12px] md:text-base text-left text-gray-700">
+                        <th className="py-1 md:py-3 px-2 md:px-4 font-semibold">COVER</th>
+                        <th className="py-1 md:py-3 px-2 md:px-4 font-semibold">TITLE</th>
+                        <th className="py-1 md:py-3 px-2 md:px-4 font-semibold">AUTHOR</th>
+                        <th className="py-1 md:py-3 px-2 md:px-4 font-semibold">GENRE</th>
+                        <th className="py-1 md:py-3 px-2 md:px-4 font-semibold flex items-center gap-1">
                             Rating
                         </th>
-                        <th className="py-3 px-4 font-semibold"></th>
+                        <th className="py-1 md:py-3 px-2 md:px-4font-semibold"></th>
                     </tr>
                 </thead>
                 <tbody>
